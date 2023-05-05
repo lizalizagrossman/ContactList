a140038ab3ae33eac115be514088e39c80e034dc
Liza Grossman <lisok.sam@gmail.com>
2023-05-05 20:05:17 +0300

    Initial Commit

---
+++ ContactList/ContactListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactListApp.swift
+//  ContactList
+//
+//  Created by Elizabeth on 05/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct ContactListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
